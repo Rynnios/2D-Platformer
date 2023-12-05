a36e129d44b845b5a27e5f5f858308ad236853dc
ElliottGH <elliottcarteryt@gmail.com>
2023-12-05 04:57:01 -0500

    More UI improvements and bug fixes
    I will make the knockback and level designs tomorrow

--- Assets/Scripts/Levels/SkillTree.cs
+++ Assets/Scripts/Levels/SkillTree.cs
@@ -11,7 +11,8 @@ public class SkillTree : MonoBehaviour
     public GameObject hpBtn2;
     public GameObject cooldownBtn1;
     public GameObject cooldownBtn2;
-    //public TextMeshPro skillText;
+
+    [SerializeField] private TMP_Text skillPointsText;
 
     static public SkillTree S; //Singleton
 
@@ -22,7 +23,6 @@ public class SkillTree : MonoBehaviour
 
     private void Start()
     {
-        //PlayerLife.S.SetText();
         if (Data.S.cooldownBtn1Disabled)
             cooldownBtn1.SetActive(false);
         else if (Data.S.cooldownBtn2Disabled)
@@ -50,8 +50,9 @@ public class SkillTree : MonoBehaviour
             Data.S.cooldownBtn1Disabled = true;
             cooldownBtn1.SetActive(false);
             Data.S.skillPoints -= 1;
-            //PlayerLife.S.SetText();
-            //Player.S.cooldownReducer += 5;
+            skillPointsText.text = ": " + Data.S.skillPoints;
+
+            // Figure out what to do here
         }
     }
 
@@ -61,9 +62,10 @@ public class SkillTree : MonoBehaviour
         {
             Data.S.cooldownBtn2Disabled = true;
             cooldownBtn2.SetActive(false);
-            Data.S.skillPoints -= 1;      
-            //PlayerLife.S.SetText();
-            //Player.S.cooldownReducer += 5;
+            Data.S.skillPoints -= 1;
+            skillPointsText.text = ": " + Data.S.skillPoints;
+
+            // Figure out what to do here
         }
     }
 
@@ -74,7 +76,8 @@ public class SkillTree : MonoBehaviour
             Data.S.speedBtn1Disabled = true;
             speedBtn1.SetActive(false);
             Data.S.skillPoints -= 1;
-            //PlayerLife.S.SetText();
+            skillPointsText.text = ": " + Data.S.skillPoints;
+
             Data.S.moveSpeed += 1f;
         }
     }
@@ -86,7 +89,8 @@ public class SkillTree : MonoBehaviour
             Data.S.speedBtn2Disabled = true;
             speedBtn2.SetActive(false);
             Data.S.skillPoints -= 1;
-            //PlayerLife.S.SetText();
+            skillPointsText.text = ": " + Data.S.skillPoints;
+
             Data.S.moveSpeed += 1f;
         }
     }
@@ -98,9 +102,9 @@ public class SkillTree : MonoBehaviour
             Data.S.hpBtn1Disabled = true;
             hpBtn1.SetActive(false);
             Data.S.skillPoints -= 1;
-            //PlayerLife.S.SetText();
-            PlayerLife.S.maxHealth += 1f;
-            PlayerLife.S.currentHealth += 1f;
+            skillPointsText.text = ": " + Data.S.skillPoints;
+
+            PlayerLife.Instance.AddHealth();
         }
     }
 
@@ -111,9 +115,9 @@ public class SkillTree : MonoBehaviour
             Data.S.hpBtn2Disabled = true;
             hpBtn2.SetActive(false);
             Data.S.skillPoints -= 1;
-            //PlayerLife.S.SetText();
-            PlayerLife.S.maxHealth += 1f;
-            PlayerLife.S.currentHealth += 1f;
+            skillPointsText.text = ": " + Data.S.skillPoints;
+
+            PlayerLife.Instance.AddHealth();
         }
     }
 }
