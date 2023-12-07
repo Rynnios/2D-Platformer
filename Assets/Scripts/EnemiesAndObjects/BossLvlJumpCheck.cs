aba709daafaba9dc08b1a0bf097b16dda9bc79db
Lauryn <lrynson10@gmail.com>
2023-12-07 15:27:10 -0500

    Fixed enemy bug
    Fixed code, enemies can now get eliminated again when stepped on.
    Separated boss jump check from common enemy jump check

---
+++ Assets/Scripts/EnemiesAndObjects/BossLvlJumpCheck.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossLvlJumpCheck : MonoBehaviour
+{
+    [SerializeField] SkullBoss skullBoss;
+
+
+    private void Start()
+    {
+        skullBoss = GameObject.FindGameObjectWithTag("Boss").GetComponent<SkullBoss>();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Kills normal enemies in 1 shot
+        //If facing the boss, will reduce health of boss until 0, and then defeats the boss enemy
+        if (collision.GetComponent<EnemyHeadCheck>())
+        {
+            skullBoss.bossHealthTracker();
+            skullBoss.checkBossEnraged();
+        }
+    }
+}
--- Assets/Scripts/Player/EnemyHeadCheck.cs
+++ Assets/Scripts/Player/EnemyHeadCheck.cs
@@ -9,10 +9,11 @@ public class EnemyHeadCheck : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<PlayerFeetCheck>())
+        if (collision.GetComponent<PlayerFeetCheck>() || collision.GetComponent<BossLvlJumpCheck>())
         {
             playerRb.velocity = new Vector2(playerRb.velocity.x, 0f);
             playerRb.AddForce(Vector2.up * 600f);
         }
+
     }
 }
